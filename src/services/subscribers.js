8b96b1839feee1ecf684e5073564b4c55de5c01e
nikzayn <nikhilvaidyar1997@gmail.com>
2020-05-14 15:47:49 +0530

    Updated subscribers functionality

---
+++ src/services/subscribers.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const _ = require('lodash');
+
+const mailer = require('./mailer');
+
+// @type GET
+// @route /covid/list
+// @desc Get the data of covid cases in India
+// access PUBLIC
+function getCovidList(emailData) {
+    const api = 'https://api.covid19india.org/data.json';
+
+    function getData() {
+        axios.get(api)
+            .then(response => {
+                const result = response.data.statewise
+                statewise(result);
+            })
+            .catch(err => res.send(err))
+    }
+
+    //- Total number of cases overall in country
+    //- New Cases overall in country
+    //- Statewise Data
+    function statewise(data) {
+        const totalCasesOverall = data[0].confirmed
+        const newCasesOverall = data[0].deltaconfirmed
+
+        const stateWiseData = _.map(data, (val =>
+            stateData = {
+                state_name: val.state,
+                new_cases: val.deltaconfirmed,
+                total_cases: val.confirmed
+            }))
+
+        const apiData = {
+            totalCasesOverall,
+            newCasesOverall,
+            stateWiseData
+        }
+        mailer(emailData, apiData)
+    }
+
+    getData();
+}
+
+module.exports = getCovidList;
