742800e0e437de4b3ce3bd32d95d44bbd5dce651
nikzayn <nikhilvaidyar1997@gmail.com>
2020-05-14 18:47:52 +0530

    Mail system functionality for the app

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -2,11 +2,16 @@ const nodemailer = require("nodemailer");
 const _ = require('lodash')
 
 function mailer(data, apiData) {
-    // declare vars,
-    let fromMail = 'nikhilvaidyar1997@gmail.com';
-    let toMail = _.map(data, val => val.email);
-    let subject = 'Daily Updates for COVID-19 cases in India';
-    let html = `
+    if (_.isEmpty(data)) {
+        console.log("Empty subscriber's list")
+        return
+    }
+    else {
+        // declare vars,
+        let fromMail = 'nikhilvaidyar1997@gmail.com';
+        let toMail = _.map(data, val => val.email);
+        let subject = 'Daily Updates for COVID-19 cases in India';
+        let html = `
     <html>
     <head>
         <style>
@@ -22,57 +27,55 @@ function mailer(data, apiData) {
     </head>
         <body>
             <div>
-                <h2>Number of new cases in India yesterday - <span>${apiData.newCasesOverall}</span><h2>
+                <h2>Number of new cases in India yesterday - <span style="color: red">${apiData.newCasesOverall}</span><h2>
             </div>
             <div>
-                <h2>Total number of cases - <span>${apiData.totalCasesOverall}</span></h2>
+                <h2>Total number of cases - <span style="color: red">${apiData.totalCasesOverall}</span></h2>
             </div>
             <br>
-            <div style="width:80%">
+            <div style="width:100%">
                 <tr>
                     <th>States</th>
                     <th>New Cases</th>
                     <th>Total Cases</th>
                 </tr>
                 ${_.map(apiData.stateWiseData, (val) => (
-                    ` <tr>
+            `<tr>
                         <td>${val.state_name}</td>
                         <td>${val.new_cases}</td>
                         <td>${val.total_cases}</td>
-                      </tr>
-                    `
-                ))}
+                      </tr>`
+        )).join('')}
             </div>
         </body>
     </html>
     `
+        //auth
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'nikhilvaidyar1997@gmail.com',
+                pass: `${process.env.password}`
+            }
+        });
 
+        //email options
+        let mailOptions = {
+            from: fromMail,
+            to: toMail,
+            subject,
+            html
+        };
 
-    //auth
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'nikhilvaidyar1997@gmail.com',
-            pass: `${process.env.password}`
-        }
-    });
+        //send mail
+        transporter.sendMail(mailOptions, (error, response) => {
+            if (error) {
+                console.log(error);
+            }
+            console.log(response)
 
-    //email options
-    let mailOptions = {
-        from: fromMail,
-        to: toMail,
-        subject,
-        html
-    };
-
-    //send mail
-    transporter.sendMail(mailOptions, (error, response) => {
-        if (error) {
-            console.log(error);
-        }
-        console.log(toMail)
-        console.log(response)
-    })
+        })
+    }
 }
 
 module.exports = mailer;
